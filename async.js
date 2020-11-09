33ebe031535a9a6e483bfe209c980a2205296ced
hi <kangja1996@gmail.com>
2020-11-09 11:40:38 -0500

    progress

--- async.js
+++ async.js
@@ -1,1 +1,7 @@
-// asynchronous JavaScript. It will cover callbacks, promises, and async await syntax. These are all ways to deal with asynchronous data and what that means is asynchronous is when something's going on, but you don't want to wait until that thing is done to continue your program. You want to continue while it's happening 
+// asynchronous JavaScript. It will cover callbacks, promises, and async await syntax. These are all ways to deal with asynchronous data and what that means is asynchronous is when something's going on, but you don't want to wait until that thing is done to continue your program. You want to continue while it's happening.
+
+// So it's happening asyncrhonously rather than synchronous programming where something happens and you wait until it's completely finished until you move on to the next. 
+
+// Asynchronous programming is very relavant to JavaScript because we're often making requests to servers. Elsewhere, it can take a couple seconds to get your data back and you might not want your program to stall and wait for that data to come back. You want to keep going and keep doing something This is where callbacks come in. 
+
+// 
